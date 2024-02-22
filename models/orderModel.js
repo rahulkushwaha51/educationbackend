14e3f1328bb9ba9fbf2dde6d3adbaf4373234d3a
Rahul Kushwaha <rahulkushwaha5198@gmail.com>
2024-02-23 00:02:36 +0530

    cart product

--- models/orderModel.js
+++ models/orderModel.js
@@ -15,7 +15,7 @@ const orderItemSchema = new mongoose.Schema({
     },
     image: {
         type: String,
-        // required: true,
+        required: true,
     },
     title: {
         type: String,
